ea4a1f954087ab1e45d9778cf7af7028378dad3c
Xun6 <13617920171@163.com>
2020-04-27 23:19:47 +0800

    获取cookie 信息

--- Chapter9/src/main/java/com/httpclient/demo/cookiesForProperties/cookiesForGet.java
+++ Chapter9/src/main/java/com/httpclient/demo/cookiesForProperties/cookiesForGet.java
@@ -1,14 +1,17 @@
 package com.httpclient.demo.cookiesForProperties;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -32,10 +35,22 @@ public class cookiesForGet {
 
         // 测试访问逻辑
         HttpGet get = new HttpGet(testUrl);
-        HttpClient client = new DefaultHttpClient();
+//        HttpClient client = new DefaultHttpClient();
+        DefaultHttpClient client = new DefaultHttpClient();      //必须是DefaultHttpClient获取cookies信息
         HttpResponse response = client.execute(get);
         result = EntityUtils.toString(response.getEntity(),"utf-8");
         System.out.println(result);
+
+
+        //获取 cookies 信息
+        CookieStore store = client.getCookieStore();
+        List<Cookie> cookieList = store.getCookies();
+
+        for(Cookie cookie : cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name ="+name+"; cookie value ="+value);
+        }
     }
 
 }
