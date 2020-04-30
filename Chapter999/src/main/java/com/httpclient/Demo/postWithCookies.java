37380c11006687f503f94bb43a0d8ee02ede5b9c
Xun6 <13617920171@163.com>
2020-04-30 19:37:22 +0800

    携带cookies信息访问下一个get请求

--- Chapter999/src/main/java/com/httpclient/Demo/postWithCookies.java
+++ Chapter999/src/main/java/com/httpclient/Demo/postWithCookies.java
@@ -3,6 +3,7 @@ package com.httpclient.Demo;
 import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -20,6 +21,8 @@ public class postWithCookies {
 
     private String url;
     private ResourceBundle bundle;
+    //定义储存cookies信息的变量
+    private CookieStore store;
 
     @BeforeTest
     public void testBeforeMethod(){
@@ -43,13 +46,34 @@ public class postWithCookies {
 
 
         //获取cookies信息
-        CookieStore store = client.getCookieStore();
+        this.store = client.getCookieStore();
         List<Cookie> cookieList = store.getCookies();
 
         for(Cookie cookie : cookieList){
             String name = cookie.getName();
             String value = cookie.getValue();
             System.out.println("cookie name = "+ name+" ;cookie value = "+value);
+        }
+
+    }
+
+    @Test(dependsOnMethods = {"testGetCookies"})  //依赖 testGetCookies 方法执行
+    public void testGetWithCookie() throws IOException {
+        String result;
+        String uri = bundle.getString("testGetWithCookies.uri");
+        String TestUrl = this.url+uri;
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet get = new HttpGet(TestUrl);
+        //设置cookies信息
+        client.setCookieStore(this.store);
+        HttpResponse response = client.execute(get);
+        //获取响应的状态码
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("statusCode = "+statusCode);
+        if(statusCode == 200){
+            result = EntityUtils.toString(response.getEntity());
+            System.out.println(result);
         }
 
     }
