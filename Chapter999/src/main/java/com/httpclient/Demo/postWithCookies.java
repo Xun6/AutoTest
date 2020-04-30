f47e32f71b3e10ec25956ba692b5ceced7758e49
Xun6 <13617920171@163.com>
2020-04-30 19:03:05 +0800

    配置.porperties文件，并获取cookie信息

--- Chapter999/src/main/java/com/httpclient/Demo/postWithCookies.java
+++ Chapter999/src/main/java/com/httpclient/Demo/postWithCookies.java
@@ -1,4 +1,56 @@
 package com.httpclient.Demo;
 
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class postWithCookies {
+
+    private String url;
+    private ResourceBundle bundle;
+
+    @BeforeTest
+    public void testBeforeMethod(){
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA);
+        url = bundle.getString("test.url");
+    }
+
+
+    @Test
+    public void testGetCookies() throws IOException {
+        String result;
+        String uri = bundle.getString("testGetBackCookies.uri");
+        //拼接地址
+        String testUrl = this.url+uri;
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet get = new HttpGet(testUrl);
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity());
+        System.out.println(result);
+
+
+        //获取cookies信息
+        CookieStore store = client.getCookieStore();
+        List<Cookie> cookieList = store.getCookies();
+
+        for(Cookie cookie : cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name = "+ name+" ;cookie value = "+value);
+        }
+
+    }
 }
