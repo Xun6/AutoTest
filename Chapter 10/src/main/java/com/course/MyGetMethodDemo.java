78ba65658c958431446dac7a6381af7ae0ac41ae
Xun6 <13617920171@163.com>
2020-05-04 14:41:25 +0800

    使用StringBoot开发需携带cookies信息的get请求

--- Chapter 10/src/main/java/com/course/MyGetMethodDemo.java
+++ Chapter 10/src/main/java/com/course/MyGetMethodDemo.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @RestController
 public class MyGetMethodDemo {
@@ -19,5 +21,27 @@ public class MyGetMethodDemo {
         Cookie cookie = new Cookie("login","true");
         response.addCookie(cookie);
         return "恭喜你获取cookies信息成功";
+    }
+
+
+    /**
+     * 这是一个需要携带cookies信息才能访问的get请求
+     * 需要客户端携带cookies访问
+     * */
+
+    @RequestMapping(value="/get/with/cookies",method = RequestMethod.GET)
+    public String getWithCookies(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();    //获取cookies
+        //判断cookies为空
+        if(Objects.isNull(cookies)){
+            return "你必须携带cookies信息来访问";
+        }
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("login") && cookie.getValue().equals("true")){
+                return "恭喜你，携带cookies信息的get请求访问成功！！！";
+            }
+        }
+
+        return "你必须携带cookies信息进行访问！";
     }
 }
