bd8f7c52031963b16c85180ca44861dcba506938
Xun6 <13617920171@163.com>
2020-05-04 21:38:46 +0800

    使用StringBoot开发需携带参数的get请求（两种开发方式（url区分））

--- Chapter 10/src/main/java/com/course/MyGetMethodDemo.java
+++ Chapter 10/src/main/java/com/course/MyGetMethodDemo.java
@@ -1,12 +1,12 @@
 package com.course;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
@@ -28,7 +28,6 @@ public class MyGetMethodDemo {
      * 这是一个需要携带cookies信息才能访问的get请求
      * 需要客户端携带cookies访问
      * */
-
     @RequestMapping(value="/get/with/cookies",method = RequestMethod.GET)
     public String getWithCookies(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();    //获取cookies
@@ -44,4 +43,37 @@ public class MyGetMethodDemo {
 
         return "你必须携带cookies信息进行访问！";
     }
+
+
+    /**
+     * 开发一个需要参数才能访问的 get 请求
+     * 第一种实现方式 url：key=value & key=value
+     * 我们来模拟商品列表
+     * */
+    @RequestMapping(value="/get/with/param",method=RequestMethod.GET)
+    public Map getList(@RequestParam Integer start,@RequestParam Integer end){
+        Map<String,Integer> mylist = new HashMap<>();
+
+        mylist.put("鞋子",350);
+        mylist.put("衬衫",300);
+        mylist.put("裤子",100);
+        return mylist;
+    }
+
+
+    /**
+     * 第二种需要参数才能访问的 get 请求
+     * url: ip:port/get/with/param/10/20
+     * */
+
+    @RequestMapping(value="/get/with/param/{start}/{end}")
+    public Map myGetList(@PathVariable Integer start,@PathVariable Integer end){
+        Map<String,Integer> myList = new HashMap<>();
+        myList.put("鞋子",350);
+        myList.put("衬衫",300);
+        myList.put("裤子",100);
+        return myList;
+
+    }
+
 }
