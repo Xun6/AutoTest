ac185df272aeae4c6c3318a07cffa7e9a9f2ef94
Xun6 <13617920171@163.com>
2020-05-04 11:48:29 +0800

    StringBoot Demo运行成功了（依赖包的下载太费时了！）

---
+++ Chapter 10/src/main/java/hello/SampleController.java
@@ -0,0 +1,24 @@
+package hello;
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+//import org.springframework.boot.stereotype.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "hello world!";
+    }
+
+    public static void main(String [] args) throws Exception{
+        SpringApplication.run(SampleController.class, args);
+    }
+}
