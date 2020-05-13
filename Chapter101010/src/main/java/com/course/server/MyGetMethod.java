5aac01edea3881fb2f37668be14dcfcd4e091322
Xun6 <13617920171@163.com>
2020-05-13 20:40:21 +0800

    springboot中两种带参数get请求实现方法

--- Chapter101010/src/main/java/com/course/server/MyGetMethod.java
+++ Chapter101010/src/main/java/com/course/server/MyGetMethod.java
@@ -1,12 +1,12 @@
 package com.course.server;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 //以下是我的所有 Get方法的类
@@ -37,5 +37,37 @@ public class MyGetMethod {
             }
         }
         return "你必须携带cookies信息来访问！";
+    }
+
+    /**
+     * 开发一个需要携带参数才能访问的 get 请求
+     * 第一种实现方式 url: key=value & key=value
+     * 例如模拟获取商品列表
+     * */
+    @RequestMapping(value ="/get/with/param",method = RequestMethod.GET)
+    public Map<String,Integer> getlist(@RequestParam Integer start,@RequestParam Integer end){
+        Map<String,Integer> mylist = new HashMap<>();
+        mylist.put("鞋子",400);
+        mylist.put("chengshan",300);
+        mylist.put("wanzi",900);
+
+        return mylist;
+
+    }
+
+    /**
+     * 第二种带参数get请求实现方法
+     * url:ip:port/get/with/param/10/20
+     * */
+    @RequestMapping(value = "/get/with/param/{start}/{end}")
+    public Map myGetList(@PathVariable Integer start,
+                         @PathVariable Integer end){
+        Map<String,String> list = new HashMap<>();
+        list.put("年龄","18");
+        list.put("姓名","小鱼");
+        list.put("性别","男");
+
+        return list;
+
     }
 }
