a382b3d0a360886baebae1ca9fb4299bdc122109
Xun6 <13617920171@163.com>
2020-05-26 19:43:54 +0800

    mybatis+springboot完成第一个查询demo

---
+++ Chapter1111/src/main/java/com/xiaoyu/Application.java
@@ -0,0 +1,28 @@
+package com.xiaoyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PreDestroy;
+
+/**
+ * 这是一个启动类，第二种方法如下（不用再去扫描其他包）
+ * */
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+    private static ConfigurableApplicationContext context;
+
+    public static void main(String[] args){
+        Application.context = SpringApplication.run(Application.class,args);
+    }
+
+    //预摧毁方法
+    @PreDestroy
+    public void close(){
+        Application.context.close();
+
+    }
+}
