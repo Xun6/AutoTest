b14a8f9d7ac557c82d704e84c1015cee0b00016a
Xun6 <13617920171@163.com>
2020-05-28 19:53:13 +0800

    使用mybaits实现数据的更新和删除

--- Chapter1111/src/main/java/com/xiaoyu/controller/Demo.java
+++ Chapter1111/src/main/java/com/xiaoyu/controller/Demo.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j
 @RestController
@@ -28,13 +25,30 @@ public class Demo {
         return template.selectOne("getUserCount2");    //其中getUserCount2 是我的 mysql.xml 文件内的id
     }
 
+
     //往数据库内添加数据
     @RequestMapping(value = "/addUserData",method = RequestMethod.POST)
     @ApiOperation(value="添加用户",httpMethod = "POST")
-    //把 User 类里的数据传进来
+    //使用@Requestbody 把 User 类里的数据传进来
     public int addUser(@RequestBody User user){
         int result = template.insert("addUser2",user);    //向表中插入数据
         return result;
+    }
+
+
+    //更新数据库内的数据
+    @RequestMapping(value = "/updateUserData",method = RequestMethod.POST)
+    @ApiOperation(value = "编辑更新用户",httpMethod = "POST")
+    public int updateUser(@RequestBody User user){
+        return template.update("updateUserData2",user);
+    }
+
+
+    //删除数据库内数据
+    @RequestMapping(value = "/delUserData",method = RequestMethod.GET)
+    @ApiOperation(value ="删除用户",httpMethod = "GET")
+    public int delUser(@RequestParam int id){
+        return template.delete("delUserData2",id);
     }
 
 
---
+++ Chapter1111/src/main/java/com/xiaoyu/swaggerConfig/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.xiaoyu.swaggerConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+//swagger配置(需要先引入依赖库)
+@Configuration    //表示专门加载配置文件
+@EnableSwagger2    //引入依赖的注解
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                .paths(PathSelectors.regex("/.*"))          //正则匹配访问方法的路径
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("小鱼的接口文档")
+                .contact(new Contact("小鱼儿","","13617920171@163.com"))
+                .description("这是我的swagger生成的接口文档")
+                .version("1.0.0.1")
+                .build();
+
+    }
+}
